0af523d016f2b9759b8b4fdcc8c77ee89b89885c
Amitrajit_Bose@Mac <ar-amitrajit.bose@rakuten.com>
2020-07-02 02:47:22 +0530

    D1

---
+++ 1-Arranging-Coins.py
@@ -0,0 +1,29 @@
+def checkDirection(n, g):
+    l = (g * (g+1))//2
+    m = ((g+1) * (g+2))//2
+    if l <= n and m > n:
+        return 0
+    elif l <= n:
+        return 1
+    return 2
+
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        low, high = 0, (2**31)-1
+        while low < high:
+            mid = (low + high) // 2
+            x = checkDirection(n, mid)
+            if x == 0:
+                return mid
+            if x == 1:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return low
+
+
+"""
+Time = O(log(n))
+Space = O(1)
+"""
+
---
+++ README.md
@@ -0,0 +1,13 @@
+# LC July 2020
+
+This repository contains solution to July Challenge 2020. All the solutions have been
+authored by the repository owner `@amitrajitbose`.
+
+Please note that most of the solutions do not obey good software engineering and clean code
+practices, because they were solely submitted for the purpose of competitive programming.
+
+----------------
+
+Please Star the repository, if you have found it useful.
+
+Have a great day! Godspeed :)
